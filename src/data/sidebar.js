f98bedb761eb14c8f9ed99064b6ca3c26b9cd111
Tim Oram <dev@mitmaro.ca>
2024-09-24 20:14:01 -0230

    Add start a team and registration details

--- src/data/sidebar.js
+++ src/data/sidebar.js
@@ -1,32 +1,33 @@
 export const SIDEBAR = [
-  {
-    text: "Home",
-    link: "",
-    children: [],
-  },
-  {
-    text: "Start a Team",
-    link: "coming-soon",
-  },
-  {
-    text: "Volunteer",
-    link: "coming-soon",
-  },
-  {
-    text: "Team Resources",
-    link: "coming-soon",
-    // children: [{ text: "Archives", link: "/resources/archives" }],
-  },
-  {
-    text: "Sponsors",
-    link: "coming-soon",
-  },
-  {
-    text: "About",
-    link: "coming-soon",
-  },
-  {
-    text: "Contact Us",
-    link: "coming-soon",
-  },
+	{
+		text: "Home",
+		link: "",
+		children: [],
+	},
+	{
+		text: "Start a Team",
+		link: "start-a-team",
+		children: [{ text: "How to Register", link: "how-to-register" }],
+	},
+	{
+		text: "Volunteer",
+		link: "coming-soon",
+	},
+	{
+		text: "Team Resources",
+		link: "coming-soon",
+		// children: [{ text: "Archives", link: "/resources/archives" }],
+	},
+	{
+		text: "Sponsors",
+		link: "coming-soon",
+	},
+	{
+		text: "About",
+		link: "coming-soon",
+	},
+	{
+		text: "Contact Us",
+		link: "coming-soon",
+	},
 ];
